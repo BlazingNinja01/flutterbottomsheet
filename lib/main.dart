4d308c42d99be95a03f0788ea2f17385a3412299
Hassan Shahid <hassanshahidofficial@gmail.com>
2020-10-26 01:43:33 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:bottomsheetguide/bottomsheet_widget.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Bottom Sheet Guide',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: HomeView());
+  }
+}
+
+class HomeView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: MyFloatingButton(),
+    );
+  }
+}
+
+class MyFloatingButton extends StatefulWidget {
+  @override
+  _MyFloatingButtonState createState() => _MyFloatingButtonState();
+}
+
+class _MyFloatingButtonState extends State<MyFloatingButton> {
+  bool _show = true;
+  @override
+  Widget build(BuildContext context) {
+    return _show
+        ? FloatingActionButton(
+            onPressed: () {
+              var sheetController = showBottomSheet(
+                context: context,
+                builder: (context) => bottomSheetWidget(),
+              );
+
+              _showButton(false);
+
+              sheetController.closed.then((value) {
+                _showButton(true);
+                print('Sheet closed');
+              });
+            },
+          )
+        : Container();
+  }
+
+  void _showButton(bool value) {
+    setState(() {
+      _show = value;
+    });
+  }
+}
